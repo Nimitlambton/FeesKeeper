ad7b7780046ddcadea5a6056dbf080116d9a129b
Nimit <nimi@Nimits-MacBook-Air.local>
2020-02-20 21:35:36 -0500

    with gradelupdagte

--- app/src/main/java/com/example/labtest1/feeskeeper/nimit/addAct.kt
+++ app/src/main/java/com/example/labtest1/feeskeeper/nimit/addAct.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.activity_add.view.*
 import java.util.*
 
 
@@ -18,6 +20,7 @@ class addAct : AppCompatActivity() {
         setContentView(R.layout.activity_add)
         title = "add Details"
 
+
         var  Name = findViewById(R.id.username) as EditText
 
         var  Fees = findViewById(R.id.fee) as EditText
@@ -27,16 +30,19 @@ class addAct : AppCompatActivity() {
         val datePicker = findViewById<DatePicker>(R.id.datePicker1)
 
 
-
-
         var  date = findViewById(R.id.date) as EditText
 
         var add = findViewById(R.id.add) as Button
 
 
 
+
+
+
         date.setOnClickListener{
 
+
+            closeKeyboard()
             val today = Calendar.getInstance()
             datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
                 today.get(Calendar.DAY_OF_MONTH)
@@ -46,14 +52,21 @@ class addAct : AppCompatActivity() {
                 val msg = "$day/$month/$year"
                 System.out.println(msg)
                 date.setText(msg)
-                this.setVisible(false)
+
+
             }
+
         }
 
 
         add.setOnClickListener{
 
 
+       System.out.println("$Name.text $Fees.text +  $Age.text + $date.text")
+
+
+
+
         }
 
 
@@ -61,8 +74,24 @@ class addAct : AppCompatActivity() {
     }
 
 
+    private fun closeKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+        }
     }
 
+
+
+
+
+
+
+
+    }
 
 
 
